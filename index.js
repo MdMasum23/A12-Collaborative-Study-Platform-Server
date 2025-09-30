ce10eb608eeda8f96b31ead3a349a1592a4ed348
MdMasum23 <mdmasum67172019@gmail.com>
2025-09-30 12:43:13 +0600

    configure MongoDB client, create collections, and add Firebase token verification middleware

--- index.js
+++ index.js
@@ -26,3 +26,52 @@ admin.initializeApp({
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@phero1.w4yidho.mongodb.net/?retryWrites=true&w=majority&appName=PHERO1`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+
+        // Collection Build:
+        const usersCollection = client.db('collabStudy').collection('users');
+        const sessionsCollection = client.db('collabStudy').collection('sessions');
+        const reviewsCollection = client.db('collabStudy').collection('reviews');
+        const bookingsCollection = client.db('collabStudy').collection('bookings');
+        const notesCollection = client.db('collabStudy').collection('notes');
+        const materialsCollection = client.db('collabStudy').collection('materials');
+
+
+        // Custom Middleware
+        const verifyFBToken = async (req, res, next) => {
+            // console.log('Header in middleware', req.headers);
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            };
+
+            const token = authHeader.split(' ')[1]
+            if (!token) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            };
+
+            // Verify The Token:
+            try {
+                const decoded = await admin.auth().verifyIdToken(token)
+                req.decoded = decoded;
+                next();
+            } catch (error) {
+                return res.status(401).send({ message: 'forbidden access' })
+            }
+        };
+
+
+   
